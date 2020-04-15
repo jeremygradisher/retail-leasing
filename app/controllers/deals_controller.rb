997b663e9188a9197020c70f7c3da1d75fdd95b7
jeremygradisher <jeremygradisher@gmail.com>
2020-04-15 23:29:23 +0000

    Removed area_id from Deals to avoid confusion

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -69,6 +69,6 @@ class DealsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def deal_params
-      params.require(:deal).permit(:deal_name, :gross_area, :net_rentable_area, :lease_status, :map_id, :project_id, :area_id)
+      params.require(:deal).permit(:deal_name, :gross_area, :net_rentable_area, :lease_status, :map_id, :project_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200415225440) do
+ActiveRecord::Schema.define(version: 20200415232710) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "suite_number"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20200415225440) do
     t.string   "lease_status"
     t.integer  "project_id"
     t.integer  "map_id"
-    t.integer  "area_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.index ["map_id"], name: "index_deals_on_map_id"
