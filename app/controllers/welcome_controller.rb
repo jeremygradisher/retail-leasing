7dd9537f43f106c87412f3fa444c829e07c993b2
jeremygradisher <jeremygradisher@gmail.com>
2020-04-22 21:25:34 +0000

    Fixed must be logged in permissions on User_Projects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @projects = Project.all
+    if current_user
+      if current_user.is_admin?
+        @projects = Project.all
+      else
+        @projects = current_user.projects
+      end
+    end
   end
 end
