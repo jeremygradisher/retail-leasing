701f4287687df1030af329ef01ecab04aa7e527a
jeremygradisher <jeremygradisher@gmail.com>
2020-04-22 00:10:09 +0000

    Added role to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420171142) do
+ActiveRecord::Schema.define(version: 20200422000306) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "suite_number"
@@ -121,8 +121,10 @@ ActiveRecord::Schema.define(version: 20200420171142) do
     t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
 end
