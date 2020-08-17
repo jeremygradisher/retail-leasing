dce49c65cbd117d139e60404f8097afbbe4e337a
jeremygradisher <jeremygradisher@gmail.com>
2020-08-17 20:36:56 +0000

    Getting the charts right

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -34,6 +34,9 @@ class ProjectsController < ApplicationController
     
     @areas_deal = AreasDeal.new
     @deals = @project.deals.all
+    
+    @areasquarefootage = Area.where(project_id: params[:id]).pluck(:area_sqft)
+    @netrentablearea = @deals.pluck(:net_rentable_area)
   end
 
   # GET /projects/new
