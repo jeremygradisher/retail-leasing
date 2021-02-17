da8edadb2ba52d8eef45cd2e3673ca5992b50451
Jeremy Gradisher <jeremygradisher@gmail.com>
2021-02-17 19:37:58 +0000

    Additions to the pop-up for Space/Deal association order

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,11 +1,7 @@
 class Area < ApplicationRecord
   belongs_to :project
   belongs_to :map
-  
-  #moved these to deal
-  #has_one :schedule, dependent: :destroy
-  #has_one :workletter, dependent: :destroy
-  
+
   validates_presence_of :coords
   
   has_many :areas_deals, :dependent => :destroy
