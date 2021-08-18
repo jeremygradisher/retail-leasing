01d841adb2af0946a3d47562173f03f3373a0bf3
Jeremy Gradisher <jeremygradisher@gmail.com>
2021-08-18 18:42:38 +0000

    Changed reply email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'StatusPlan2 <no-reply@statusplan2.herokuapp.com>'
+  config.mailer_sender = 'Status Plan <no-reply@statusplanapp.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
